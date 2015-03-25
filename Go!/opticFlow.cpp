f1eb2e90d0efe0eecb4730b0e2c16ca65bca3a7b
unknown <s4284756@hs-a0850000070.hms.uq.edu.au>
2015-03-25 16:39:18 +1000

    updates

--- Go!/opticFlow.cpp
+++ Go!/opticFlow.cpp
@@ -220,11 +220,17 @@ void OpticFlow::createCubePoly(osg::Vec3Array *p, const double radius)
 		const int START = (i - p->begin()) * temp->getNumElements();
 
 		osg::ref_ptr<osg::DrawElementsUInt> de = new osg::DrawElementsUInt(GL_QUAD_STRIP);
-		de->push_back(0 + START); de->push_back(1 + START); 
-		de->push_back(3 + START); de->push_back(2 + START);
-		de->push_back(4 + START); de->push_back(5 + START);
-		de->push_back(7 + START); de->push_back(6 + START);
-		de->push_back(0 + START); de->push_back(1 + START);
+// 		de->push_back(0 + START); de->push_back(1 + START); 
+// 		de->push_back(3 + START); de->push_back(2 + START);
+// 		de->push_back(4 + START); de->push_back(5 + START);
+// 		de->push_back(7 + START); de->push_back(6 + START);
+// 		de->push_back(0 + START); de->push_back(1 + START);
+		de->push_back(6 + START); de->push_back(1 + START);
+		de->push_back(7 + START); de->push_back(0 + START);
+		de->push_back(4 + START); de->push_back(3 + START);
+		de->push_back(5 + START); de->push_back(2 + START);
+		de->push_back(6 + START); de->push_back(1 + START);
+
 		geom->addPrimitiveSet(de);
 
 		++i;
